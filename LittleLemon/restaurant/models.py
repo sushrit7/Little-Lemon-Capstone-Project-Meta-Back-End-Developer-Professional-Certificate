68cf74b9620c03f364beb23c44957eaaf715ae36
“Sushrit <sushrit.k@gmail.com>
2024-01-03 12:25:25 -0600

    Added Booking and Menu models

--- LittleLemon/restaurant/models.py
+++ LittleLemon/restaurant/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length = 255)
+    no_of_guests = models.IntegerField()
+    bookingDate = models.DateTimeField()
+
+class Menu(models.Model):
+    title = models.CharField(max_length = 255)
+    price = models.IntegerField()
+    inventory = models.IntegerField()
